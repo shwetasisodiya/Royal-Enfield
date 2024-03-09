c23ab56c41dd01281ce4b201797070d3b6783ea0
shwetasisodiya <sisodiyashweta5@gmail.com>
2024-03-09 11:46:55 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
         <div className="w-full bg-zinc-800 grid grid-cols-4" style={{height: "157px"}}>
             
         </div>
-      <div className='w-full bg-neutral-300' style={{height: "472px"}}>
-
+      <div className='w-full bg-neutral-300' style={{height: "472px", display:"flex", justifyContent:"center", alignItems:"center"}}>
+      @copyright 2024
       </div>
     </div>
   )
